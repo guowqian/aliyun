195d2d60665ae155103fc154513c3b3700a838ab
zhangsan <zhangsan@163.com>
2015-11-01 19:53:38 +0800

    update something

--- aliyun.java
+++ aliyun.java
@@ -1,5 +1,8 @@
 public class aliyuntest{
 	public  static void main(String[]args){
 		System.out.print("this is just a test");
+		//zhangsan update something!
+		System.out.print("i want commit too!");
+		
 	}
 }
